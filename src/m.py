58421e423e51fc0269c9685cc4106b2551d6ea1e
SamuelHathcock <samuelnhath>
2021-04-27 00:34:02 -0700

    minor shit

--- src/m.py
+++ src/m.py
@@ -7,6 +7,6 @@ import requests
 def request_image(name, day, satellite, product, time, zoomlevel, row_column):
     baseURL = 'https://rammb-slider.cira.colostate.edu'
     src = '/data/imagery' + day + satellite + product + time + zoomlevel + row_column
-
     resourceLink = baseURL + src
+    
     urllib.request.urlretrieve(resourceLink, name)
